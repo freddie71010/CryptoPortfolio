9f30fc1a32661471470d1baf037ec3c75f2dd50c
freddie71010 <freddie71010@users.noreply.github.com>
2018-03-28 15:27:15 -0400

    new column: trans_time

--- local_db_celery_test/create_db.py
+++ local_db_celery_test/create_db.py
@@ -2,15 +2,16 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_connection(db_file):
+
+def create_connection():
 	""" create a database connection to the SQLite database
 	specified by db_file
 	:param db_file: database file
 	:return: Connection object or None
 	"""
+	database = "gdax.db"
 	try:
-		conn = sqlite3.connect(db_file)
-		print("Created database...")
+		conn = sqlite3.connect(database)
 		return conn
 	except Error as e:
 		print(e)
@@ -29,16 +30,20 @@ def create_table(conn, create_table_sql):
 		print(e)
 
 def main():
-	db = "gdax.db"
-	sql_create_price_tbl = """CREATE TABLE IF NOT EXISTS prices (
+	drop_prices_tbl = """DROP TABLE prices;"""
+	sql_create_price_tbl = """
+							CREATE TABLE prices (
 								id integer PRIMARY KEY,
 								exchange text NOT NULL,
 								coin text NOT NULL,
 								price float NOT NULL,
-								trading_pair text NOT NULL
+								trading_pair text NOT NULL,
+								trans_time text NOT NULL
 								);"""
-	conn = create_connection(db)
+	conn = create_connection()
 	if conn is not None:
+		create_table(conn, drop_prices_tbl)
+		print("Dropped table: prices")
 		create_table(conn, sql_create_price_tbl)
 		print("Created table: prices")
 	else:

1dfc2745d6b4331090758c39c886fa8aefaa89f8
freddie71010 <freddie71010@users.noreply.github.com>
2018-04-02 14:31:32 -0400

    Removed old celery_gdax file. Created init.py, db_connections.py. Two template files: base, graph.html. Added route '/graph/<coin>' which grabs correctly coin data from DB and displays on page.

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
+import os
 from flask import Flask
+from flask import request, session, g, redirect, url_for, abort, \
+	render_template, flash
+from db_connections import *
+
 app = Flask(__name__)
+# Load default config and override config from an environment variable
+app.config.update(dict(
+	DATABASE=os.path.join(app.root_path, 'local_db_celery_test/gdax.db'),
+	SECRET_KEY='5wutojw3;4oiw4nrasfigjh24waor',
+	USERNAME='admin',
+	PASSWORD='default'
+))
 
 
-@app.route('/hello')
-def hello_world():
-	return "hello World"
+@app.route('/graph')
+def graph_all():
+	db = get_db()
+	query = '''
+		SELECT price, trans_time 
+		FROM prices
+		'''
+	coin_data = db.execute(query)
+	return render_template('graph.html', coin_data=coin_data, coin='All')
+
+
+@app.route('/graph/<coin>')
+def graph(coin):
+	db = get_db()
+	query = '''
+		SELECT price, trans_time 
+		FROM prices
+		WHERE coin = ?
+		'''
+	args = (coin.upper(),)
+	coin_data = db.execute(query, args)
+	return render_template('graph.html', coin_data=coin_data, coin=coin)
 
 
 @app.route('/')
 def index():
-	return "index page"
+	return render_template('base.html')
 
 
 if __name__ == '__main__':

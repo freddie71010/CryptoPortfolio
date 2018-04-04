f547c43b72ad27cfb167f08fb003637a6ed68b97
freddie71010 <freddie71010@users.noreply.github.com>
2018-04-03 22:04:39 -0400

    Consolidated dash graph code into main app.py and removed dash_graph.py and db_connections.py

--- app.py
+++ app.py
@@ -2,16 +2,14 @@ import os
 from flask import Flask
 from flask import request, session, g, redirect, url_for, abort, \
 	render_template, flash
-import db_connections
-
+import sqlite3
 from dash import Dash
-from dash_graph import setup_dash_app
-
+from dash.dependencies import Input, State, Output
+import dash_core_components as dcc
+import dash_html_components as html
 
 flask_app = Flask(__name__)
 dash_app = Dash(server=flask_app, url_base_pathname='/dash')
-
-dash_app = setup_dash_app(dash_app)
 
 # Load default config and override config from an environment variable for flask
 flask_app.config.update(dict(
@@ -21,10 +19,75 @@ flask_app.config.update(dict(
 	PASSWORD='default'
 ))
 
+# ====================================================================================
+
+dash_app.layout = html.Div(children=[
+	html.P(children=["Hello P, these are children",
+		html.H1("Hello H1, these are children"),
+	]),
+
+	dcc.Graph(
+		id='cum-graph',
+		figure={
+			'data': [
+				{'x': [1, 2, 3, 4], 'y': [4, 1, 2, 6], 'type': 'bar', 'name': 'SF'},
+				{'x': [1, 2, 3, 4], 'y': [2, 4, 5, 2], 'type': 'bar', 'name': u'Montréal'},
+			],
+			'layout': {
+				'title': 'Cumulative Graph',
+				'plot_bgcolor': '#7FDBFF',
+			},
+		},
+	),
+
+	html.Label('Dropdown'),
+	dcc.Dropdown(
+		options=[{'label': x, 'value': x} for x in ['BTC', 'ETH', 'LTC']],
+	),
+
+	dcc.Input(id='input', value='Enter something', type='text'),
+	html.Div(id='output')
+])
+
+@dash_app.callback(
+	Output(component_id='output', component_property='children'),
+	[Input(component_id='input', component_property='value')]
+)
+def update_value(input_data):
+	try:
+		return str(float(input_data)**2)
+	except:
+		return "Some error"
+
+
+# ====================================================================================
+def connect_db():
+	"""Connects to the specific database."""
+	rv = sqlite3.connect(flask_app.config['DATABASE'])
+	rv.row_factory = sqlite3.Row
+	return rv
+
+
+def get_db():
+	"""Opens a new database connection if there is none yet for the
+	current application context.
+	"""
+	if not hasattr(g, 'gdax_db'):
+		g.gdax_db = connect_db()
+	return g.gdax_db
+
+
+@flask_app.teardown_appcontext
+def close_db(error):
+	"""Closes the database again at the end of the request."""
+	if hasattr(g, 'gdax_db'):
+		g.gdax_db.close()
+# ====================================================================================
+
 
 @flask_app.route('/graph')
 def graph_all():
-	db = db_connections.get_db()
+	db = get_db()
 	query = '''
 		SELECT price, trans_time 
 		FROM prices
@@ -35,7 +98,7 @@ def graph_all():
 
 @flask_app.route('/graph/<coin>')
 def graph(coin):
-	db = db_connections.get_db()
+	db = get_db()
 	query = '''
 		SELECT price, trans_time 
 		FROM prices
